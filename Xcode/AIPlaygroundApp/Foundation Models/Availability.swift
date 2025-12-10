403e33c803868e2c1adc61eaddae09abd0047288
Ethan Huang <ethanhuang13@icloud.com>
2025-12-10 21:46:50 +0800

    Availability

--- Xcode/AIPlaygroundApp/Foundation Models/Availability.swift
+++ Xcode/AIPlaygroundApp/Foundation Models/Availability.swift
@@ -1,8 +1,27 @@
-//
-//  Availability.swift
-//  AIPlaygroundApp
-//
-//  Created by ethanhuang on 2025/12/10.
-//
+// From https://developer.apple.com/events/resources/code-along-205/
+// 1.3: Handling model availability
 
-import Foundation
+import FoundationModels
+import Playgrounds
+
+#Playground {
+  let model = SystemLanguageModel.default
+
+  // The availability property provides detailed information on the model's state.
+  switch model.availability {
+  case .available:
+    print("Foundation Models is available and ready to go!")
+
+  case .unavailable(.deviceNotEligible):
+    print("The model is not available on this device.")
+
+  case .unavailable(.appleIntelligenceNotEnabled):
+    print("Apple Intelligence is not enabled in Settings.")
+
+  case .unavailable(.modelNotReady):
+    print("The model is not ready yet. Please try again later.")
+
+  case .unavailable(let other):
+    print("The model is unavailable for an unknown reason.")
+  }
+}
---
+++ Xcode/AIPlaygroundApp/Foundation Models/Instructions.swift
@@ -0,0 +1,8 @@
+//
+//  Instructions.swift
+//  AIPlaygroundApp
+//
+//  Created by ethanhuang on 2025/12/10.
+//
+
+import Foundation
