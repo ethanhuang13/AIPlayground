1597e07e9513cd6a3e6fe563a0af59551d751e4c
Ethan Huang <ethanhuang13@icloud.com>
2025-12-10 22:34:33 +0800

    Instructions

--- Xcode/AIPlaygroundApp/Foundation Models/Instructions.swift
+++ Xcode/AIPlaygroundApp/Foundation Models/Instructions.swift
@@ -1,33 +1,21 @@
 import FoundationModels
 import Playgrounds
 
-fileprivate let prompt = "學習 Swift 程式語言的策略？"
+private let prompt = "學習 Swift 程式語言的策略？"
 
 #Playground("Instructions") {
   let instructions = """
     你是一名資深軟體工程師與講師，樂於分享經驗與知識。
     """
-  let model = SystemLanguageModel.default
   let session = LanguageModelSession(
-    model: model,
-    instructions: instructions,
+    instructions: instructions
   )
 
-  let response = try await session.respond(
-    to: prompt
-  )
+  let response = try await session.respond(to: prompt)
 }
 
 #Playground("No instructions") {
-  let instructions = """
-    """
-  let model = SystemLanguageModel.default
-  let session = LanguageModelSession(
-    model: model,
-    instructions: instructions,
-  )
+  let session = LanguageModelSession()
 
-  let response = try await session.respond(
-    to: prompt
-  )
+  let response = try await session.respond(to: prompt)
 }
