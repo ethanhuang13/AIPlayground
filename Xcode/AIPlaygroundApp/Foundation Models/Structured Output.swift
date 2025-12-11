b2bcb29f88165f4145d58243a6e4e7101958eee4
Ethan Huang <ethanhuang13@icloud.com>
2025-12-12 00:06:11 +0800

    Structured Output

--- Xcode/AIPlaygroundApp/Foundation Models/Structured Output.swift
+++ Xcode/AIPlaygroundApp/Foundation Models/Structured Output.swift
@@ -3,29 +3,17 @@ import Playgrounds
 
 @Generable
 private struct Step {
-  var number: Int
+  @Guide(description: "標題")
   var title: String
+  @Guide(description: "描述")
   var description: String
-  var example: String
+  @Guide(description: "步驟順序")
+  var number: Int
 }
 
 private let prompt = "學習 Swift 程式語言的策略？"
 
-#Playground("Non-structured") {
-  let instructions = """
-    你是一名資深軟體工程師與講師，樂於分享經驗與知識。
-    """
-  let session = LanguageModelSession(
-    instructions: instructions
-  )
-
-  let response = try await session.respond(
-    to: prompt,
-    options: GenerationOptions(sampling: .greedy)
-  )
-}
-
-#Playground("Structured") {
+#Playground("Generate steps") {
   let instructions = """
     你是一名資深軟體工程師與講師，樂於分享經驗與知識。
     """
